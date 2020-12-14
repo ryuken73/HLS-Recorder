37fa360a7116d95c98f2a3febe7792d0c3cd6df6
ryuken73 <ryuken73@gmail.com>
2020-12-15 00:43:26 +0900

    UI: modify Header (configuration icon located inside header)

--- app/components/Header.js
+++ app/components/Header.js
@@ -6,19 +6,29 @@ import SettingsIcon from '@material-ui/icons/Settings';
 
 const Header = () => {
     return (      
-        <Box display="flex" alignItems="center">
-            <BorderedBox 
+        <Box 
+            display="flex" 
+            alignItems="center"
+            bgcolor="#2d2f3b"
+            mx="5px"
+            mt="15px"
+            alignContent="center"
+            justifyContent="space-between"
+        >
+            <Box width="100px">
+                
+            </Box>
+            <Box 
                 fontFamily="Roboto, Helvetica, Arial, sans-serif" 
                 textAlign="center" 
                 fontSize="35px"
-                bgcolor="#2d2f3b"
-                mx="5px"
-                mt="15px"
-                py="10px"
-                width="95%"
+                // mx="5px"
+                // mt="15px"
+                // py="10px"
+                // width="95%"
             >CCTV Recorder
-            </BorderedBox>
-            <Box mr="auto" mt="10px">
+            </Box>
+            <Box width="100px">
                 <IconButton aria-label="configuration">
                     <SettingsIcon 
                         fontSize="large"
